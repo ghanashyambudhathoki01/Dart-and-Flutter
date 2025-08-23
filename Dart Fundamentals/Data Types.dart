25aa5a8af23034268c3726dc01820194e3e12ddd
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-23 10:52:38 +0545

    Add files via upload

---
+++ Dart Fundamentals/Data Types.dart
@@ -0,0 +1,22 @@
+void main() {
+  // int  whole numbers
+  int age = 25;
+  print("Age: $age");
+
+  // double  decimal numbers
+  double height = 5.9;
+  print("Height: $height");
+
+  // num  can be int or double
+  num score1 = 90;      // int
+  num score2 = 95.5;    // double
+  print("Scores: $score1 and $score2");
+
+  // bool  true or false
+  bool isStudent = true;
+  print("Is student? $isStudent");
+
+  // String  text
+  String name = "Samraj";
+  print("Name: $name");
+}
---
+++ Dart Fundamentals/variable.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  // Take input from user
+  var a = stdin.readLineSync();   // First input
+  var b = stdin.readLineSync();   // Second input
+
+  print("I am $a and 20 $b years old");
+}
+
+//This is a called variable in dart.

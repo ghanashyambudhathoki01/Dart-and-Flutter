3a39c6235b371f12d924fe3a83993219a3e44861
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-28 07:43:38 +0545

    Add files via upload

---
+++ Dart Fundamentals/Parameters/Arrowfunction.dart
@@ -0,0 +1,7 @@
+import "dart:io";
+void main() {
+  int square(int n) => n * n;
+  String greet(String name) => "hello, $name";
+  print(square(5));
+  print(greet("Ghanashyam"));
+}
---
+++ Dart Fundamentals/Parameters/Positional.dart
@@ -0,0 +1,7 @@
+import "dart:io";
+void introduce(String name, int age) {
+  print("My name is $name and I am $age years old.");
+}
+void main() {
+  introduce("Ghanashyam", 20);
+}
